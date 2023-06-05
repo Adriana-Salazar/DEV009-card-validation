2a98e6970888734d67f91317b123f31e3ed3f312
Adriana-Salazar <adrianabsalazar@gmail.com>
2023-06-05 17:22:52 -0500

    correccion dependencias y codeo js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import validator from './validator.js';
 
 console.log(validator);
+
+const input = document.getElementById("tarjeta")
+
+input.addEventListener("input",()=>{
+  const inputValue = document.getElementById("tarjeta").value;
+  console.log(inputValue);
+})
