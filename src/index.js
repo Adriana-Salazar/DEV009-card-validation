962e778b0b83204a663dd90fb1ef13c3ab8837a1
Adriana-Salazar <adrianabsalazar@gmail.com>
2023-06-12 09:53:27 -0500

    Funciones isValid, Maskify y Luhn

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 import validator from './validator.js';
-
+// eslint-disable-next-line no-console
 console.log(validator);
 
 const input = document.getElementById("tarjeta")
 
-input.addEventListener("input",()=>{
-  const inputValue = document.getElementById("tarjeta").value;
+input.addEventListener("input", () => {
+  const inputValue = input.value;
+  // eslint-disable-next-line no-console
   console.log(inputValue);
 })
+
+function validarTarjeta() {
+  const creditCardNumber = input.value;
+  const isValid = validator.isValid(creditCardNumber);
+  const maskedNumber = validator.maskify(creditCardNumber);
+  // eslint-disable-next-line no-console
+  console.log(`El número de tarjeta ${maskedNumber} es válido: ${isValid}`);
+  
+}
+
+const validateButton = document.getElementById('validar');
+validateButton.addEventListener('click', validarTarjeta);
+export default {};
+// eslint-disable-next-line no-console
+console.log(validator);
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,32 @@
 const validator = {
-  // ...
-};
+  // ......Convertir el número de tarjeta de crédito en un array
+  isValid: function (creditCardNumber) {
+    const digits = creditCardNumber.split("");
+    const reversedDigits = digits.reverse();
 
+    //..Aplicar el Algoritmo de Luhn
+    let sum = 0;
+    for (let i = 0; i < reversedDigits.length; i++) {
+      let digit = parseInt(reversedDigits[i], 10);
+      if (i % 2 !== 0) {
+        digit *= 2;
+        if (digit > 9) {
+          digit -= 9;
+        }
+      }
+      sum += digit;
+    }
+    return sum % 10 === 0;
+  },
+  // Verificar si la longitud de la tarjeta es menor o igual a 4
+  maskify: function (creditCardNumber) {
+    if (creditCardNumber.length <= 4) {
+      return creditCardNumber;
+    }
+    // Reemplazar todos los caracteres excepto los últimos 4 por '#'
+    const maskedNumber = creditCardNumber.slice(0, -4).replace(/./g, "#");
+    // Concatenar los últimos 4 caracteres
+    return maskedNumber + creditCardNumber.slice(-4);
+  },
+};
 export default validator;
