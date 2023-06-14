3e56d4d19eaf02158dc6295e80397543595cdf8b
Adriana-Salazar <adrianabsalazar@gmail.com>
2023-06-13 20:22:52 -0500

    Version final

--- src/index.js
+++ src/index.js
@@ -1,13 +1,5 @@
 import validator from "./validator.js";
-
 const camp = document.getElementById("tarjeta");
-//camp.addEventListener("click", function () {
-// const spaces= "";
-//if(spaces === "") {
-//document.querySelector(".empty").innerHTML = `Introduce número de tarjeta`
-//}
-//})
-
 const btn = document.getElementById("validar");
 btn.addEventListener("click", function () {
   const creditCardNumber = camp.value;
@@ -16,10 +8,22 @@ btn.addEventListener("click", function () {
   if (isValid === true) {
     document.querySelector(
       ".result"
-    ).innerHTML = `El número de tarjeta ${maskedNumber} es Válido`;
+    ).innerHTML = `El número de tarjeta ${maskedNumber} es Válido, Por favor ingrese Nombres y Apellidos`;
   } else if (isValid === false) {
     document.querySelector(
       ".result"
     ).innerHTML = `El número de tarjeta ${maskedNumber} es Invalido`;
   }
 });
+
+document.getElementById("Datos").addEventListener("click", validarDatos);
+function validarDatos() {
+  let nombres = document.getElementById("name").value;
+  let apellidos = document.getElementById("last_name").value;
+  if (nombres === "" || apellidos === "") {
+    alert("Por favor, complete los campos de nombres y apellidos.");
+  } else {
+    // Los campos están llenos, puedes enviar el formulario aquí.
+    alert("Formulario enviado correctamente.");
+  }
+}
